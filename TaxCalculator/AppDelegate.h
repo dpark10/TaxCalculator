4ed51eda4c5ae09872d4f45fc4b6ba91eb06a742
dp <dp@dps-macbook-pro.local>
2016-03-06 13:13:55 -0500

    Initial Commit

---
+++ TaxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalculator
+//
+//  Created by dp on 3/6/16.
+//  Copyright © 2016 Dan Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
